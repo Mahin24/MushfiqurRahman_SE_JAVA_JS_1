6dbf4be74babf58d02d9554047e971b88772121a
Mahin24 <mahin242424@gmail.com>
2020-10-30 15:28:50 +0600

    logout system add

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,6 +1,14 @@
-import React, {Fragment} from 'react'
+import React, {Fragment} from 'react';
 
 function Navbar() {
+
+    const logout = () => {
+        localStorage.removeItem("myProfile");
+        setTimeout(() => {
+            window.location = "/auth/login";
+        })
+    };
+
     return (
         <Fragment>
         <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
@@ -16,22 +24,10 @@ function Navbar() {
             <li className="nav-item dropdown no-arrow">
                 <a className="nav-link dropdown-toggle" href="#" id="userDropdown"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span className="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                    <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60"/>
+                    <img className="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" />
                 </a>
             {/* <!-- Dropdown - User Information --> */}
             <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                {/* <a className="dropdown-item" href="#">
-                <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                Profile
-                </a>
-                <a className="dropdown-item" href="#">
-                <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                Settings
-                </a>
-                <a className="dropdown-item" href="#">
-                <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                Activity Log
-                </a> */}
                 {/* <div className="dropdown-divider"></div> */}
                 <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                 <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -57,7 +53,7 @@ function Navbar() {
                     <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                     <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button type="button" class="btn btn-primary" onClick={logout}>Logout</button>
                     </div>
                 </div>
                 </div>
